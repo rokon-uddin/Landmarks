b31d0b4a07c2304960d1ba550051ad2899b115e6
Mohammed Rokon Uddin <rokon.uddin@monstar-lab.com>
2024-07-05 10:20:35 +0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mohammed Rokon Uddin on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
